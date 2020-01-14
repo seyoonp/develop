9eea1449275fe5d868f4f32c96e929f3aefe967c
HOME\park.seyoon <seyoonp@gmail.com>
2020-01-14 16:13:51 +0900

    다국어 처리, 사이트환경 설정값 thymleaf 로 출력, 에러페이지 처리

--- varzac/src/com/varzac/controller/CommonController.java
+++ varzac/src/com/varzac/controller/CommonController.java
@@ -7,7 +7,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
@@ -98,7 +100,6 @@ public class CommonController {
 	
 	/**
 	 * 파일 다운로드 컨트롤러
-	 * 
 	 * @param fileIdx 파일 데이터 고유값
 	 * @param response 서버로부터 파일정보를 응답받을 객체
 	 * @throws Exception
@@ -131,5 +132,54 @@ public class CommonController {
         // response를 중지하고 닫아준다.
         response.getOutputStream().flush();
         response.getOutputStream().close();
+	}
+	
+	/**
+	 * PathVariable 을 이용한 에러페이지 공통화 처리
+	 * @param request
+	 * @param error_code
+	 * @return
+	 */
+	@RequestMapping(value="/common/error{error_code}")
+	public ModelAndView error(HttpServletRequest request, @PathVariable String error_code) {
+		
+		ModelAndView mv = new ModelAndView("/common/error");
+		String msg = (String) request.getAttribute("javax.servlet.error.message"); 
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("STATUS_CODE", request.getAttribute("javax.servlet.error.status_code"));
+		map.put("REQUEST_URI", request.getAttribute("javax.servlet.error.request_uri"));
+		map.put("EXCEPTION_TYPE", request.getAttribute("javax.servlet.error.exception_type"));
+		map.put("EXCEPTION", request.getAttribute("javax.servlet.error.exception"));
+		map.put("SERVLET_NAME", request.getAttribute("javax.servlet.error.servlet_name"));
+		
+		try {
+			int status_code = Integer.parseInt(error_code);
+			switch (status_code) {
+				case 400: msg = "잘못된 요청입니다."; break;
+				case 403: msg = "접근이 금지되었습니다."; break;
+				case 404: msg = "페이지를 찾을 수 없습니다."; break;
+				case 405: msg = "요청된 메소드가 허용되지 않습니다."; break;
+				case 500: msg = "서버에 오류가 발생하였습니다."; break;
+				case 503: msg = "서비스를 사용할 수 없습니다."; break;
+				default: msg = "알 수 없는 오류가 발생하였습니다."; break;
+			}
+		} catch(Exception e) {
+			msg = "기타 오류가 발생하였습니다.";
+		} finally {
+			map.put("MESSAGE", msg);
+		}
+		
+    	if(map.isEmpty() == false ) {
+    		Iterator<Entry<String, Object>> iterator = map.entrySet().iterator();
+    		Entry entry = null;
+    		while(iterator.hasNext()) {
+    			entry = iterator.next();
+    			logger.info("key : "+entry.getKey()+", value : "+entry.getValue());
+    		}
+    	}
+		
+    	mv.addObject("error", map);
+		return mv;
 	}
 }
--- varzac/src/com/varzac/controller/MessageController.java
+++ varzac/src/com/varzac/controller/MessageController.java
@@ -1,21 +1,19 @@
 package com.varzac.controller;
 
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 
-import org.codehaus.groovy.tools.shell.util.MessageSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import com.varzac.vo.HelloVo;
-import com.varzac.vo.NoticeVo;
 
 @Controller
 @RequestMapping("/message")
@@ -27,7 +25,29 @@ public class MessageController {
 	@Autowired 
 	private MessageSource messageSource; //message-context.xml 에 선언되어있는 bean id 값
 	
+	@Value("#{config['file.upload.path']}")
+	private String fileUploadPath;
+	
 	private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 	
-	
+	@RequestMapping(value = "/i18n", method = RequestMethod.GET)
+	public String i18n(Locale locale, HttpServletRequest request, Model model) {
+		
+		// RequestMapingHandler로 부터 받은 Locale 객체를 출력해 봅니다. 
+		logger.info("Welcome i18n! The client locale is {}.", locale);
+		
+		// localeResolver 로부터 Locale 을 출력해 봅니다.
+		logger.info("Session locale is {}.", localeResolver.resolveLocale(request));
+
+		logger.info("site.title : {}", messageSource.getMessage("site.title", null, "default text", locale));
+		logger.info("site.count : {}", messageSource.getMessage("site.count", new String[] {"첫번째"}, "default text", locale));
+		logger.info("not.exist : {}", messageSource.getMessage("not.exist", null, "default text", locale)); 
+		//logger.info("not.exist 기본값 없음 : {}", messageSource.getMessage("not.exist", null, locale));
+		
+		// JSP 페이지에서 EL 을 사용해서 arguments 를 넣을 수 있도록 값을 보낸다.
+		model.addAttribute("siteCount", messageSource.getMessage("msg.first", null, locale));
+		model.addAttribute("fileUploadPath",fileUploadPath);
+		
+		return "/lang/i18n";
+	}
 }
